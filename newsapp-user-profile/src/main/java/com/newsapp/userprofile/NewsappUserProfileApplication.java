3c9f56eaa89b5f5d8eecdaca73a4327a04983f4b
chandan <chandankalluri9@gmail.com>
2023-12-26 17:52:59 +0530

    commit-2 : Basic swagger added(v2)

--- newsapp-user-profile/src/main/java/com/newsapp/userprofile/NewsappUserProfileApplication.java
+++ newsapp-user-profile/src/main/java/com/newsapp/userprofile/NewsappUserProfileApplication.java
@@ -1,9 +1,17 @@
 package com.newsapp.userprofile;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "${spring.application.name}",
+				description = "This service helps user to register to News Application."
+		)
+)
 public class NewsappUserProfileApplication {
 
 	public static void main(String[] args) {
--- newsapp-wishlist-service/src/main/java/com/newsapp/wishlist/NewsappWishlistServiceApplication.java
+++ newsapp-wishlist-service/src/main/java/com/newsapp/wishlist/NewsappWishlistServiceApplication.java
@@ -1,9 +1,17 @@
 package com.newsapp.wishlist;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "${spring.application.name}",
+				description = "This service helps user to wishlist the news articles that user liked or wanted to visit them again."
+		)
+)
 public class NewsappWishlistServiceApplication {
 
 	public static void main(String[] args) {
